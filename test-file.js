fbb18ecc3b94255fc6634c23693d08bbe093e882
Harshit Malik <harshit.m@statusneo.com>
2025-10-13 02:19:02 +0530

    Test commit with pre-commit hook

---
+++ test-file.js
@@ -0,0 +1,8 @@
+// This is a test file for the pre-commit hook
+function calculateSum(a, b) {
+  // This function adds two numbers
+  return a + b;
+}
+
+// Test the function
+console.log('Sum of 2 and 3 is:', calculateSum(2, 3));
