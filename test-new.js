865fff85900918503943e2e124362e14e64e2100
Harshit Malik <harshit.m@statusneo.com>
2025-10-13 12:47:07 +0530

    Add test-new.js to test pre-commit hook

---
+++ test-new.js
@@ -0,0 +1,7 @@
+// A simple function to test the pre-commit hook
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// Test the function
+console.log(greet('World')); // Should output: Hello, World!
