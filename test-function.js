d68ecf42a7376a33656855c8dc5008046dccdd84
Harshit Malik <harshit.m@statusneo.com>
2025-10-13 12:46:24 +0530

    Test updated pre-commit hook

---
+++ test-function.js
@@ -0,0 +1,8 @@
+// A simple function with potential improvements
+function add(a, b) {
+  // This function adds two numbers
+  return a + b;
+}
+
+// Test the function
+console.log(add(5, 3)); // Should output: 8
