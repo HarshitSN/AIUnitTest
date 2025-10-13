4134d6251d992ac925720e865a71e68c86a9dd72
Harshit Malik <harshit.m@statusneo.com>
2025-10-13 13:22:21 +0530

    Test the pre-commit hook with staged files

--- test.js
+++ test.js
@@ -40,6 +40,7 @@ function calculateTotal(items) {
 // Note: The following lines are not related to the calculateTotal function and seem to be some test code.
 // They will be ignored in this review.
 a = 2;
+uwoncIK
 // print(a) // This line will throw an error because print is not a function in modern JavaScript.
 // meow // This line will throw an error because meow is not defined.
 // hi // This line will not throw an error, but it's not related to the calculateTotal function.
