0df989036d800cda0cf94d89dcdfb54ef1df2eb8
Harshit <harshitmalik29@gmail.com>
2025-10-16 13:20:44 +0530

    try 7

--- cal.js
+++ cal.js
@@ -67,7 +67,7 @@ class Calculator {
 const calc = new Calculator();
 calc.appendNumber(5);
 calc.chooseOperation('+');
-calc.appendNumber(3);
+calc.appendNumber(4);
 calc.compute();
 
 console.log('Result:', calc.getDisplayValue()); // Output: Result: 8
