711724467697fa0cdf2fa7ac3aa53b99282a75a1
Harshit <harshitmalik29@gmail.com>
2025-10-17 16:46:55 +0530

    hopefully fixed

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,6 @@
 function isPalindrome(str) {
-    const cleaned = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-    return cleaned === cleaned.split('').reverse().join('');
+  const cleaned = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  return cleaned === cleaned.split('').reverse().join('');
 }
 
 module.exports = { isPalindrome };
