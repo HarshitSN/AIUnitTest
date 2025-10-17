f81df5c530673ff1a867071496577214853765a9
Harshit <harshitmalik29@gmail.com>
2025-10-17 16:34:44 +0530

    ok

--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,6 @@
 function isPalindrome(str) {
-  // Remove non-alphanumeric characters and convert to lowercase
-  const cleaned = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-  // Check if the string is equal to its reverse
-  return cleaned === cleaned.split('').reverse().join('');
+    const cleaned = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    return cleaned === cleaned.split('').reverse().join('');
 }
 
 module.exports = { isPalindrome };
